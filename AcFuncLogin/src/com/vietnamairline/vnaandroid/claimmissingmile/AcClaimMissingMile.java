add334805462696da945bf051880fea376a5f431
hunglefpt <hunglv1@fsoft.com.vn>
2013-04-13 16:38:08 +0700

    add header file

--- AcFuncLogin/src/com/vietnamairline/vnaandroid/claimmissingmile/AcClaimMissingMile.java
+++ AcFuncLogin/src/com/vietnamairline/vnaandroid/claimmissingmile/AcClaimMissingMile.java
@@ -1,3 +1,10 @@
+/*
+ * Name: $RCSfile: AcClaimMissingMile.java,v $
+ * Version: $Revision: 1.00 $
+ * Date: $Date: 2013/04/10 18:17:50 $
+ *
+ * Copyright (C) 2013 FPT Software. All rights reserved.
+ */
 package com.vietnamairline.vnaandroid.claimmissingmile;
 
 import android.app.Activity;
