89e2786826f41ab5082eb780c1dcd40a6527f4d7
hunglefpt <hunglv1@fsoft.com.vn>
2013-04-13 16:34:41 +0700

    add header file

--- AcFuncLogin/src/com/vietnamairline/vnaandroid/startup/AcFuncHome.java
+++ AcFuncLogin/src/com/vietnamairline/vnaandroid/startup/AcFuncHome.java
@@ -1,3 +1,10 @@
+/*
+ * Name: $RCSfile: AcFuncHome.java,v $
+ * Version: $Revision: 1.00 $
+ * Date: $Date: 2013/04/10 18:17:50 $
+ *
+ * Copyright (C) 2013 FPT Software. All rights reserved.
+ */
 package com.vietnamairline.vnaandroid.startup;
 
 import android.app.Activity;
